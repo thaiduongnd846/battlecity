f7cdfcf4430d35e7d5aeeab57aa5841543983444
nguyenxuanhung96 <nguyenxuan.hung96@gmail.com>
2016-06-18 18:38:47 +0700

    update1

--- src/com/actions/NewClass.java
+++ src/com/actions/NewClass.java
@@ -10,5 +10,7 @@ package com.actions;
  * @author Nguyen Xuan Hung
  */
 public class NewClass {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
